09446bc809853ede2f412d778b9e3c9640eab34e
jose2899 <jose.rios@unl.edu.ec>
2024-02-02 17:58:49 -0500

    implementacion modulo 2.1

--- app/modulo/transformador_datos.py
+++ app/modulo/transformador_datos.py
@@ -9,59 +9,56 @@ def transformar_datos(estudiante, bitacoras):
 
     # Transformar el sexo
     sexo = 1 if estudiante.genero.upper() == 'M' else 0
-    print('genero', sexo)
+
+    edad = estudiante.edad
 
     # Transformar las inasistencias (asistencias)
     inasistencias = bitacoras.filter(asistencias='No').count()
-    print('---------asistenciassss ------------', inasistencias)
+
     # Calcular el total de terapias (sumar inasistencias)
-    total_terapias = sum([bitacora.observacion_conducta.count('excelente') +
+    totalTerapias = sum([bitacora.observacion_conducta.count('excelente') +
                       bitacora.observacion_conducta.count('buena') +
                       bitacora.observacion_conducta.count('regular')
                       for bitacora in bitacoras])
 
-    print('---------total terapia ------------',total_terapias)
 
     # Calcular la conducta
     total_excelente = sum([bitacora.observacion_conducta.count('excelente') for bitacora in bitacoras])
     total_buena = sum([bitacora.observacion_conducta.count('buena') for bitacora in bitacoras])
     total_regular = sum([bitacora.observacion_conducta.count('regular') for bitacora in bitacoras])
 
-    conducta = (total_excelente * 1.5) + (total_buena * 1) + (total_regular * 0.5)
+    conducta_ponderada = (total_excelente * 1.5) + (total_buena * 1) + (total_regular * 0.5)
     
-    print('---------total conducta ------------',conducta)
     # Calcular los temas trabajados
-    temas_trabajados = {}
     temas = ['m', 'vocales', 'fonemas', 'fonologia', 'escritura', 'p', 'lectura', 'dictado', 's', 'l', 'n', 'd', 'b', 't', 'g']
+    temas_trabajados = {tema: 0 for tema in temas}
     
-    for tema in temas:
-        temas_trabajados[tema] = 0
-
-
-    # Iterar sobre cada bitacora en el conjunto de consultas (QuerySet)
+    # Iterar sobre cada bitácora en el conjunto
     for bitacora in bitacoras:
-    # Separar los temas trabajados en una lista de letras
+        # Separar los temas trabajados en una lista de letras
         temas_formulario = bitacora.temas_trabajados.replace(' ', '').lower().split(',')
-    
+
+        # Incrementar el contador solo para los temas que están en la lista 'temas'
         for tema in temas_formulario:
             tema = tema.strip()
             if tema in temas:
                 temas_trabajados[tema] += 1
-    
-    print('---------temas ------------',temas_trabajados)
+
+   # Imprimir resultados
+    for tema, recuento in temas_trabajados.items():
+        print(f'{tema}: {recuento}')
     # Calcular el avance
     avance_mejorando = sum([bitacora.avance.count('mejorando') for bitacora in bitacoras])
     avance_regular = sum([bitacora.avance.count('regular') for bitacora in bitacoras])
     avance_retroceso = sum([bitacora.avance.count('retroceso') for bitacora in bitacoras])
 
-    avance = (avance_mejorando * 1.5) + (avance_regular * 1) + (avance_retroceso * 0.5)
+    avance_ponderado = (avance_mejorando * 1.5) + (avance_regular * 1) + (avance_retroceso * 0.5)
 
-    print('---------total avance ------------',avance)
-    avances_dataset.append(avance)
+    avances_dataset.append(avance_ponderado)
     # Calcular la conducta total del niño actual
 
     # Agregar la conducta del niño actual a la lista de conductas del dataset
-    conductas_dataset.append(conducta)
+    conductas_dataset.append(conducta_ponderada)
     
 
     # Clasificar en categorías según cuartiles
@@ -69,44 +66,34 @@ def transformar_datos(estudiante, bitacoras):
     q1 = df['Total'].quantile(0.25)
     q2 = df['Total'].median()
     q3 = df['Total'].quantile(0.75)
-    categoria_conducta = pd.cut(df['Total'], bins=[float('-inf'), q1, q2, float('inf')], labels=['Bajo', 'Medio', 'Alto']).values[-1]
+    conducta_total = pd.cut(df['Total'], bins=[float('-inf'), q1, q2, float('inf')], labels=['Bajo', 'Medio', 'Alto']).values[-1]
 
-    print('---------categoria de la conducta ------------',categoria_conducta)
 
     # Clasificar en categorías según cuartiles para avance
     df_avance = pd.DataFrame(avances_dataset, columns=['Total'])
     q1_avance = df_avance['Total'].quantile(0.25)
     q2_avance = df_avance['Total'].median()
     q3_avance = df_avance['Total'].quantile(0.75)
-    categoria_avance = pd.cut(df_avance['Total'], bins=[float('-inf'), q1_avance, q2_avance, float('inf')],labels=['Bajo', 'Medio', 'Alto']).values[-1]
-    
-    print('---------categoria avance ------------',categoria_avance)
+    avance_total = pd.cut(df_avance['Total'], bins=[float('-inf'), q1_avance, q2_avance, float('inf')],labels=['Bajo', 'Medio', 'Alto']).values[-1]
+
 
     # Mapear las categorías a valores numéricos para avance
     mapeo_categorias_avance = {'Bajo': 1, 'Medio': 2, 'Alto': 3}
-    valor_categoria_avance = mapeo_categorias_avance[categoria_avance]
-
-    print('---------valor del avance ------------',valor_categoria_avance)
+    avance = mapeo_categorias_avance[avance_total]
 
     # Mapear las categorías a valores numéricos
     mapeo_categorias = {'Bajo': 1, 'Medio': 2, 'Alto': 3}
-    conducta_final = mapeo_categorias[categoria_conducta]
+    conducta = mapeo_categorias[conducta_total]
     
-    print('---------valor conducta ------------',conducta_final)
     # Crear el diccionario con los datos transformados
     datos_transformados = {
         'sexo': sexo,
-        'edad': estudiante.edad['edad'],
+        'edad': edad,
         'inasistencias': inasistencias,
-        'total_terapias': total_terapias,
+        'totalTerapias': totalTerapias,
         'conducta': conducta,
         'avance': avance,
-        'temas_trabajados': temas_trabajados,
-        'categoria_conducta': categoria_conducta,  # Nueva variable
-        'categoria_avance': categoria_avance,
-        'conducta_final': conducta_final,
-        'valor_categoria_avance': valor_categoria_avance,
-        # Agrega otras variables aquí según sea necesario
+        **temas_trabajados
     }
 
     return datos_transformados
