8eb569ccd7215e994db48d2e3f58f6fd0ac79147
jose2899 <jose.rios@unl.edu.ec>
2024-02-26 16:21:04 -0500

    mejorando modulo 1

--- app/modulo/views.py
+++ app/modulo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.http import JsonResponse
 from app.usuarios.usuario.models import Estudiante
 from app.terapiass.models import Diagnostico
@@ -16,6 +17,7 @@ modelo_optimizado = joblib.load('C:/Users/ANDRES RIOS/tesisModelo/modelo_optimiz
 
 
 def detalle_estudiante_tdah(request, estudiante_id = None):
+    transformacion_exitosa = False
     if estudiante_id:
         estudiante = get_object_or_404(Estudiante, pk=estudiante_id)
         planificaciones = Planificacion.objects.filter(estudiante=estudiante)
@@ -28,34 +30,39 @@ def detalle_estudiante_tdah(request, estudiante_id = None):
         }
 
         if request.method == 'POST':
-            # Extraer datos del formulario
-            bitacoras = NuevaBitacora.objects.filter(bitacora__estudiante=estudiante)
+            estudiante_id = request.POST.get('estudiante_id')
+            if estudiante_id:
+                # Extraer datos del formulario
+                estudiante = get_object_or_404(Estudiante, pk=estudiante_id)
+                bitacoras = NuevaBitacora.objects.filter(bitacora__estudiante=estudiante)
+                print("se realizo la transformacion")
+                # Llamar a transformar_datos con los argumentos adecuados
+                datos_transformados = transformar_datos(estudiante, bitacoras)
+    
 
-            # Llamar a transformar_datos con los argumentos adecuados
-            datos_transformados = transformar_datos(estudiante, bitacoras)
+                # Realizar la predicción con el modelo
+                prediccion = modelo_optimizado.predict(pd.DataFrame([datos_transformados]))[0]
 
-            # Realizar la predicción con el modelo
-            prediccion = modelo_optimizado.predict(pd.DataFrame([datos_transformados]))[0]
+                # Obtengo la importancia de las características (temas) del modelo
+                importancias_temas = modelo_optimizado.feature_importances_
+                temas = ['m', 'vocales', 'fonemas', 'fonologia', 'escritura', 'p', 'lectura', 'dictado', 's', 'l', 
+                        'n', 'd', 'b', 't', 'g']
+                temas_importancia = dict(zip(temas, importancias_temas))
+                temas_ordenados = sorted(temas_importancia.keys(), key=lambda x: temas_importancia[x], reverse=True)
+                N_temas_importantes = 5
+                temas_relevantes = temas_ordenados[:N_temas_importantes]
 
-            # Obtengo la importancia de las características (temas) del modelo
-            importancias_temas = modelo_optimizado.feature_importances_
-            temas = ['m', 'vocales', 'fonemas', 'fonologia', 'escritura', 'p', 'lectura', 'dictado', 's', 'l', 
-                    'n', 'd', 'b', 't', 'g']
-            temas_importancia = dict(zip(temas, importancias_temas))
-            temas_ordenados = sorted(temas_importancia.keys(), key=lambda x: temas_importancia[x], reverse=True)
-            N_temas_importantes = 5
-            temas_relevantes = temas_ordenados[:N_temas_importantes]
+                print("Importancia de las características (temas):")
+                for tema in temas_relevantes:
+                    importancia = temas_importancia[tema]
+                    print(f"{tema}: {importancia}")
 
-            print("Importancia de las características (temas):")
-            for tema in temas_relevantes:
-                importancia = temas_importancia[tema]
-                print(f"{tema}: {importancia}")
-
-            # Agregar la información de la predicción al contexto
-            context.update({'prediccion': prediccion, 'temas_relevantes': temas_relevantes})
-            
-
+                # Agregar la información de la predicción al contexto
+                context.update({'prediccion': prediccion, 'temas_relevantes': temas_relevantes})
+                transformacion_exitosa = True
+        context['transformacion_exitosa'] = transformacion_exitosa
         return render(request, 'modulo/fases_proceso.html', context)
     else:
         estudiantes_tdah = Planificacion.objects.filter(diagnostico__nombre_diagnostico='TDAH', estudiante__edad__range=(7, 8)).select_related('estudiante', 'diagnostico')
         return render(request, 'modulo/fases_proceso.html', {'estudiantes_tdah': estudiantes_tdah})
+    
