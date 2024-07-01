64bf77afc699b2a0384f58638a4e0dcad8d8e7d7
jose2899 <jose.rios@unl.edu.ec>
2024-07-01 12:24:24 -0500

    despliegue 3.0

--- proyectAprendiendo/settings.py
+++ proyectAprendiendo/settings.py
@@ -64,14 +64,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'proyectAprendiendo.middleware.LoginRequiredMiddleware',
+    #'proyectAprendiendo.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'proyectAprendiendo.urls'
