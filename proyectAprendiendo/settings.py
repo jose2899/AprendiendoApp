f28f982cba928aaecf4aaba3560bd7f77ee3f3df
jose2899 <jose.rios@unl.edu.ec>
2024-06-30 19:14:44 -0500

    despliegue3.0

--- proyectAprendiendo/settings.py
+++ proyectAprendiendo/settings.py
@@ -164,11 +164,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+# Archivos estáticos y multimedia
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #LOGIN
 LOGIN_REDIRECT_URL = 'index'
